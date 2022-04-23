258c1cba1923b42b95045223a76f968e14108e95
vikalp varshney <99534514+vikalpvarshney1@users.noreply.github.com>
2022-04-23 14:47:08 +0530

    .

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,4 +1,4 @@
-import one from '../assets/png/Screenshot 2022-03-17 145530.png'
+import one from '../bigbasket.png'
 import two from '../assets/png/Screenshot 2022-03-21 162917.png'
 import three from '../assets/png/Screenshot 2022-03-17 153015.png'
 import four from '../assets/svg/projects/four.svg'
@@ -6,80 +6,81 @@ import five from '../assets/svg/projects/five.svg'
 import six from '../assets/svg/projects/six.svg'
 import seven from '../assets/svg/projects/seven.svg'
 import eight from '../assets/svg/projects/eight.svg'
-
+import img from '../book.png'
+// import book from './'
 
 export const projectsData = [{
-        id: 1,
-        projectName: 'Bigbasket (Clone)',
-        projectDesc: 'In this project we have tried to make a look alike clone of Bigbasket. With our efforts and the technology stack, that we have learned so far in the masai school, we were able to clone the front end with high precision and quality.',
-        tags: ['javascript', 'CSS', 'HTML'],
-        code: 'https://github.com/vikalpvarshney1/bigBasketCloneProject',
-        demo: 'https://bigbasketteam11clone.netlify.app/index.html',
-        image: one
-    },
-    {
-        id: 2,
-        projectName: 'Electric-Car',
-        projectDesc: 'Front-end Website based on Electric Cars.',
-        tags: ['HTML', 'CSS', 'JS'],
-        code: 'https://github.com/vikalpvarshney1/stage-1-project',
-        demo: 'https://electric-car-project.netlify.app/',
-        image: two
-    },
-    {
-        id: 3,
-        projectName: '',
-        projectDesc: '',
-        tags: [],
-        code: '',
-        demo: '',
-        image: ''
-    },
-    {
-        id: 4,
-        projectName: '',
-        projectDesc: '',
-        tags: [],
-        code: '',
-        demo: '',
-        image: ''
-    },
-    {
-        id: 5,
-        projectName: '',
-        projectDesc: '',
-        tags: [],
-        code: '',
-        demo: '',
-        image: ''
-    },
-    {
-        id: 6,
-        projectName: '',
-        projectDesc: '',
-        tags: [],
-        code: '',
-        demo: '',
-        image: ''
-    },
-    {
-        id: 7,
-        projectName: '',
-        projectDesc: '',
-        tags: [],
-        code: '',
-        demo: '',
-        image: ''
-    },
-    {
-        id: 8,
-        projectName: '',
-        projectDesc: '',
-        tags: [],
-        code: '',
-        demo: '',
-        image: ''
-    },
+    id: 1,
+    projectName: 'Bigbasket (Clone)',
+    projectDesc: 'In this project we have tried to make a look alike clone of Bigbasket. With our efforts and the technology stack, that we have learned so far in the masai school, we were able to clone the front end with high precision and quality.',
+    tags: ['javascript', 'CSS', 'HTML'],
+    code: 'https://github.com/vikalpvarshney1/bigBasketCloneProject',
+    demo: 'https://bigbasketteam11clone.netlify.app/index.html',
+    image: one
+},
+{
+    id: 2,
+    projectName: 'Electric-Car',
+    projectDesc: 'Front-end Website based on Electric Cars.',
+    tags: ['HTML', 'CSS', 'JS'],
+    code: 'https://github.com/vikalpvarshney1/stage-1-project',
+    demo: 'https://electric-car-project.netlify.app/',
+    image: two
+},
+{
+    id: 3,
+    projectName: 'Bookmyshow',
+    projectDesc: 'About BookMyShow website is basically for entertainment purpose, In this app, a user can book movie tickets by choosing the different theatres and timings accordingly. The user also can see some events happening around them and can attend the events.',
+    tags: ['React'],
+    code: 'https://github.com/vikalpvarshney1/team8',
+    demo: '',
+    image: img
+},
+{
+    id: 4,
+    projectName: '',
+    projectDesc: '',
+    tags: [],
+    code: '',
+    demo: '',
+    image: ''
+},
+{
+    id: 5,
+    projectName: '',
+    projectDesc: '',
+    tags: [],
+    code: '',
+    demo: '',
+    image: ''
+},
+{
+    id: 6,
+    projectName: '',
+    projectDesc: '',
+    tags: [],
+    code: '',
+    demo: '',
+    image: ''
+},
+{
+    id: 7,
+    projectName: '',
+    projectDesc: '',
+    tags: [],
+    code: '',
+    demo: '',
+    image: ''
+},
+{
+    id: 8,
+    projectName: '',
+    projectDesc: '',
+    tags: [],
+    code: '',
+    demo: '',
+    image: ''
+},
 
 ]
 
